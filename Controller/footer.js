d36c77c2e96deee7988f9728a4ae90746e36dda4
tsaristbomba <andrexerdna@gmail.com>
2021-07-04 12:52:36 -0300

    netlify config

--- Controller/footer.js
+++ Controller/footer.js
@@ -1,4 +1,4 @@
 const year = new Date().getFullYear();
 const footer = document.getElementById("footer");
 
-footer.innerHTML = `©${year} Andrei T. Ferreira. <a href="/Client/about.html">About</a>`;
+footer.innerHTML = `©${year} Andrei T. Ferreira. <a href="/Client/src/about.html">About</a>`;
