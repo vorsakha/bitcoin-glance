e18c35612db0ec20277b09cf0a8a8cad67ef39dc
tsaristbomba <andrexerdna@gmail.com>
2021-07-04 12:15:09 -0300

    about page

--- src/index.js
+++ src/index.js
@@ -2,14 +2,9 @@ import { getBtc } from "../Controller/Api/binanceData.js";
 import { handleSignal } from "../Controller/Signal/signal.js";
 
 // Content
-const footer = document.getElementById("footer");
 const signal4h = document.getElementById("signal-4h");
 const signal1d = document.getElementById("signal-1d");
 const signal1w = document.getElementById("signal-1w");
-
-const year = new Date().getFullYear();
-
-footer.innerText = `©${year} Andrei T. Ferreira. All rights reserved.`;
 
 // Get websocket Binance data
 getBtc();
